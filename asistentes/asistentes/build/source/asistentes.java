618f9c55aba634aefc8618cbb993784d790029e0
Nicolás Troncoso <hi@nicotron.cl>
2019-02-20 18:52:34 -0300

    archivos para exploción en Processing

--- asistentes/asistentes/build/source/asistentes.java
+++ asistentes/asistentes/build/source/asistentes.java
@@ -1,8 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class asistentes extends PApplet {
+
 Table table, santiago, valdivia, concepcion, patagonia;
 ArrayList<Table> tables = new ArrayList<Table>();
 
-void setup() {
-
+public void setup() {
+  
   table = loadTable("asistentes.csv", "header");
 
   tables.add(table);
@@ -10,9 +26,9 @@ void setup() {
   tables.add(valdivia);
   tables.add(concepcion);
   tables.add(patagonia);
-
+  
   println(tables.get(0));
-
+  
   for(int i=0; i < tables.size(); i++){
     //tables[i].addColumn("mesa");
     //table[i].addColumn("nombre");
@@ -20,22 +36,31 @@ void setup() {
     //table[i].addColumn("facilitador");
   
   }
-
-  println(table.getRowCount() + " filas total");
-
+  
+  println(table.getRowCount() + " filas total"); 
+  
   for (TableRow row : table.rows()) {
-
+    
     String sede = row.getString("sede");
     String mesa = row.getString(1);
     String nombre = row.getString("nombre");
     String rol = row.getString("rol");
     String facilitador = row.getString("facilitador");
-
+    
     if (sede.equals("Puerto Montt")){
       println(sede + "\t" + mesa + "\t" + nombre);
     } else {
     println(sede + "\t\t" + mesa + "\t" + nombre);
     }
   }
-
+  
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "asistentes" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
